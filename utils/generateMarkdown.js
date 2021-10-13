e7c5e0a5d3a5535be6031bef8a9cd4a2c2ec0cad
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-13 00:30:39 -0700

    added badge map

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,61 @@
 const fs = require('fs');
+
+// Badge Data
+const badgeData = [ {
+      "name": "Apache",
+      "badge": "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  },
+  {
+      "name": "Boost",
+      "badge": "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  },
+  {
+      "name": "BSD",
+      "badge": "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)" 
+  },
+  {
+      "name": "Creative Commons",
+      "badge": "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)"
+  },
+  {
+      "name": "Eclipse Public",
+      "badge": "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
+  },
+  {
+      "name": "GNU",
+      "badge": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+  },
+  {
+      "name": "IBM",
+      "badge": "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+  },
+  {
+      "name": "MIT",
+      "badge": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  },
+  {
+      "name": "Mozilla",
+      "badge": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  },
+  {
+      "name": "Perl",
+      "badge": "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)"
+  } ];
+
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(!license) return "";
 
+  const badges = new Map();
+  console.log(badgeData);
+  for(const {name, badge} of badgeData)  {
+    badges.set(name, badge);
+  }
+
+  console.log(badges);
 }
 
 // TODO: Create a function that returns the license link
