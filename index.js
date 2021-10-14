685e4e41af3cc8b2f4740acfc8cc9f863c38704d
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-14 11:07:47 -0700

    added questions and contributorQuestions

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "title",
+        message: "Input the project title: "
+    },
+    {
+        name: "description",
+        message: "Input the project description: "
+    },
+    {
+        name: "installation",
+        message: "Input the installation instructions: "
+    },
+    {
+        name: "usage",
+        message: "Input the usage information: "
+    },
+    {
+        name: "license",
+        type: "list",
+        message: "Choose a license: ",
+        choices: [
+            "Apache",
+            "Boost",
+            "BSD",
+            "Creative Commons",
+            "Eclipse Public",
+            "GNU",
+            "IBM",
+            "MIT",
+            "Mozilla",
+            "Perl"
+        ]
+    },
+    {
+        name: "tests",
+        message: "Input your programs tests: "
+    }
+];
+
+const contributorQuestions = [
+    {
+        name: "contributorName",
+        message: "Input a contributor name: "
+    },
+    {
+        name: "contributorGitHub",
+        message: "Input a contributor GitHub: "
+    },
+    {
+        name: "contributorLinkedIn",
+        message: "Input a contributor LinkedIn"
+    },
+    {
+        type: "confirm",
+        name: "askAgain",
+        message: "are there any other contributors? (hit Enter for Yes)",
+        default: true
+    }
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
